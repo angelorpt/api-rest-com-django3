3a55f0ac9b10b8ec0f1326fca38608975fa24155
Angelo Rafael <angelorpt@gmail.com>
2021-02-21 12:50:10 -0400

    add | Recurso - Listando alunos matriculados em um curso

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
 from rest_framework    import viewsets, generics
 from escola.models     import Aluno, Curso, Matricula
-from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo lista de alunos"""
@@ -23,3 +23,10 @@ class ListaMatriculasAluno(generics.ListAPIView):
         queryset = Matricula.objects.filter(aluno_id = self.kwargs['pk'])
         return queryset
     serializer_class = ListaMatriculasAlunoSerializer
+
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Listando alunos matriculados no curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id = self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
